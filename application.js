eea42d401d6435f17aeb4a355adcf54cb2dae33f
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-01-01 13:09:14 +0200

    responsive bugs

--- application.js
+++ application.js
@@ -8,7 +8,7 @@ const app = express();
 const ejsMate = require("ejs-mate");
 const AppError = require("./Utils/apperror");
 const mongoSanitize = require("express-mongo-sanitize");
-const { formdataPolyfill } = require('formdata-polyfill');
+import 'formdata-polyfill'
 
 const campgroundRoutes = require("./routes/camproutes");
 const reviewRoutes = require("./routes/reviewroutes");
