b07a9cb6f438886f212582ffcedbbbfdaba786ed
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-01-31 11:54:48 +0200

    snitcher 1.5 new branch

--- application.js
+++ application.js
@@ -20,7 +20,7 @@ const cookieParser = require("cookie-parser");
 const User = require("./models/user");
 const passport = require("passport");
 const Strategy = require("passport-local");
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/YelpCamp';
+const dbUrl = 'mongodb://localhost:27017/YelpCamp' || process.env.DB_URL;
 
 const MongoStore = require("connect-mongo")(session);
 
