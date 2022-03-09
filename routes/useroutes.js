fbf81c2af030a67d2bf920aaf75ad8d9c5bba34d
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-03-09 18:02:14 +0200

    test

--- routes/useroutes.js
+++ routes/useroutes.js
@@ -8,10 +8,10 @@ const { cloudStore } = require("../cloudstorage/main");
 const multer = require("multer");
 const upload = multer({ storage: cloudStore });
 
-router.route("/register").post(((req, res, next) => {
+router.route("/register").post(upload.single('profileImage'), ((req, res, next) => {
     console.log(req.body);
     next();
-}), upload.single('profileImage'), wrapAsync(registerUser));
+}), wrapAsync(registerUser));
 router.get("/users", wrapAsync(users))
 router.route("/login")
     .post(passport.authenticate('local', { failureFlash: true, failureRedirect: "/login" }), loggedIn);
