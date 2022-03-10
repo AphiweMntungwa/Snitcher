58eb80b86664a0ccb86772118b044aedda9bec98
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-03-10 23:58:25 +0200

    test

--- application.js
+++ application.js
@@ -69,7 +69,7 @@ sessionConfig = {
     cookie: {
         httpOnly: true,
         expires: Date.now() * 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24
+        maxAge: 1000 * 60 * 60
     }
 }
 app.use(session(sessionConfig));
