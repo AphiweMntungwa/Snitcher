7fbe1712c61d36a782ef92539157de54abf33955
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-03-11 11:41:32 +0200

    test

--- application.js
+++ application.js
@@ -71,9 +71,10 @@ sessionConfig = {
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        expires: Date.now() * 1000 * 60 * 60 * 24 * 7,
+        _expires: Date.now() * 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24,
-        sameSite: 'None'
+        sameSite: 'None',
+        secure: false
     }
 }
 app.use(session(sessionConfig));
