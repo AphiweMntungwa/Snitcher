555a59a03a67b7ab8aad59259587e443e8cd7344
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-04-09 14:26:20 +0200

    bugs

--- application.js
+++ application.js
@@ -18,7 +18,7 @@ const User = require("./models/user");
 const passport = require("passport");
 const Strategy = require("passport-local");
 
-const dbUrl = 'mongodb://localhost:27017/YelpCamp' || process.env.DB_URL
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/YelpCamp'
 const session = require('express-session')
 
 app.use(express.static(path.join(__dirname, 'build')))
