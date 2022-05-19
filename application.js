5851f1876426ec6f01249dce638970390991b712
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-05-19 19:24:12 +0200

    bugs

--- application.js
+++ application.js
@@ -20,7 +20,7 @@ console.log('6 a')
 
 
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/YelpCamp'
+const dbUrl = '' //process.env.DB_URL || 'mongodb://localhost:27017/YelpCamp'
 
 const campgroundRoutes = require("./routes/postroutes");
 const commentRoutes = require("./routes/commentRoutes");
