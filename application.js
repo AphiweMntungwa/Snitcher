315d5e6ad5a561d075cc0610ad2482b3751be2ea
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-05-21 00:31:47 +0200

    session

--- application.js
+++ application.js
@@ -54,6 +54,7 @@ app.use(session({
     saveUninitialized: true,
     resave: true,
     cookie: {
+        sameSite: true,
         httpOnly: false,
         secure: false,
         maxAge: Date.now() * 1000 * 60 * 60 * 24 * 7,
