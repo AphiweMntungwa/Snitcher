b2640fed2364a9789198a52fc16fd9c2b94eb05e
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-05-20 21:54:19 +0200

    session

--- application.js
+++ application.js
@@ -52,9 +52,9 @@ app.use(session({
     name: 'bagade',
     secret: 'istherestillhope',
     saveUninitialized: true,
-    resave: false,
+    resave: true,
     cookie: {
-        sameSite: 'None',
+        sameSite: 'none',
         secure: true,
         maxAge: Date.now() * 1000 * 60 * 60 * 24 * 7,
     }
